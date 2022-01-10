a28b60341ccb71ece56f7d76b6fe1ec433638882
Vlad Prodan <vladprodan6@gmail.com>
2022-01-10 17:20:29 +0200

    Setting up Sessions

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from "@nestjs/common";
+const CookieSession = require("cookie-session")
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(CookieSession({
+    name: 'session',
+    keys: ['key1', 'key2']
+  }))
   app.useGlobalPipes(
       new ValidationPipe({
         whitelist: true
