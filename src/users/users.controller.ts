75f07537900632a9bc6dc6ac0d1ff28ea1308696
Vlad Prodan <vladprodan6@gmail.com>
2022-01-10 18:36:26 +0200

    Getting the Current User

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -14,14 +14,9 @@ export class UsersController {
                 private authService: AuthService) {
     }
 
-    @Get("/colors/:color")
-    setColor(@Param('color') color: string, @Session() session: any) {
-        session.color = color
-    }
-
-    @Get("/colors")
-    getColor(@Session() session: any) {
-        return session.color
+    @Get("/get-me")
+    async getMe(@Session() session: any) {
+        return await this.userService.findOne(session.userId);
     }
 
     @Post("/signup")
