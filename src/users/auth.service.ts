a77685739a0980f530c941429b22a7b8f48b3a07
Vlad Prodan <vladprodan6@gmail.com>
2022-01-10 16:02:02 +0200

    Implementing Signup Functionality

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,11 +1,29 @@
-import {Injectable} from '@nestjs/common';
+import {BadRequestException, Injectable} from '@nestjs/common';
 import {UsersService} from './users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private repository: UsersService) {}
+    constructor(private repository: UsersService) {
+    }
 
-  public isAuthenticated(): boolean {
-    return !!localStorage.getItem('token');
-  }
+    public isAuthenticated(): boolean {
+        return !!localStorage.getItem('token');
+    }
+
+    async signup(email: string, password: string) {
+        // See if email is in use
+        const users = await this.repository.find(email);
+        if (users.length) {
+            throw new BadRequestException('Email in use');
+        }
+
+        // Hash the user password
+
+
+        return this.repository.create(email, password);
+    }
+
+    signin(user: any) {
+        return this.repository.(user);
+    }
 }
