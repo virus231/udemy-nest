a104e7c67615a7d5b8aaa978184389e58d8c94f1
Vlad Prodan <vladprodan6@gmail.com>
2022-01-11 15:07:04 +0200

    applying Dotenv for Config

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,16 +6,30 @@ import {ReportsModule} from './reports/reports.module';
 import {TypeOrmModule} from '@nestjs/typeorm';
 import { UserEntity } from './users/user.entity';
 import {ReportEntity} from "./reports/report.entity";
-
+import {ConfigModule, ConfigService} from "@nestjs/config";
 
 @Module({
     imports: [
-        TypeOrmModule.forRoot({
-            type: "sqlite",
-            database: "db.sqlite",
-            entities: [UserEntity, ReportEntity],
-            synchronize: true,
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: `.env.${process.env.NODE_ENV}`,
         }),
+        TypeOrmModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                type: 'sqlite',
+                database: configService.get<string>('DB_NAME'),
+                entities: [UserEntity, ReportEntity],
+                synchronize: true,
+            }),
+            imports: [ConfigService],
+        }),
+        // TypeOrmModule.forRoot({
+        //     type: "sqlite",
+        //     database: "db.sqlite",
+        //     entities: [UserEntity, ReportEntity],
+        //     synchronize: true,
+        // }),
         UsersModule, ReportsModule],
     controllers: [AppController],
     providers: [AppService],
