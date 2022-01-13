1a46620f498fb370c1da894ff4f843ac49eba4aa
Vlad Prodan <vladprodan6@gmail.com>
2022-01-13 15:33:56 +0200

    Receiving Report Creation Requests

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import {CreateReportDto} from "./dtos/create-report.dto";
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+    create(data: CreateReportDto) {
+        return data;
+    }
+
+}
