e4b837965445193957665c3fd42d385aad69dee9
Vlad Prodan <vladprodan6@gmail.com>
2022-01-13 15:14:47 +0200

    A DTO for Report Creation

---
+++ src/reports/dtos/create-report.dto.ts
@@ -0,0 +1,16 @@
+export class CreateReportDto {
+    price: number;
+
+    company: string;
+
+    model: string;
+
+    year: number;
+
+    longitude: number;
+
+    latitude: number;
+
+    mileage: number;
+
+}
