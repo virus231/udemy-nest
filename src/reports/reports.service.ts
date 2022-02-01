835befcdbc9200ba2d26c24c505a9e04413ba496
Vlad Prodan <vladprodan6@gmail.com>
2022-02-01 11:49:29 +0200

    Writing a Query to Produce the Estimate

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,35 +1,41 @@
-import {Injectable, NotFoundException} from '@nestjs/common';
-import {InjectRepository} from '@nestjs/typeorm';
-import {Repository} from 'typeorm';
-import {Report} from './report.entity';
-import {CreateReportDto} from './dtos/create-report.dto';
-import {User} from '../users/user.entity';
-import { GetEstimateDto } from './dtos/get-estimate.dto';
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { Report } from "./report.entity";
+import { CreateReportDto } from "./dtos/create-report.dto";
+import { User } from "../users/user.entity";
+import { GetEstimateDto } from "./dtos/get-estimate.dto";
 
 @Injectable()
 export class ReportsService {
-    constructor(@InjectRepository(Report) private repo: Repository<Report>) {
-    }
+  constructor(@InjectRepository(Report) private repo: Repository<Report>) {
+  }
 
-    createEstimate(estimateDto: GetEstimateDto) {
-        return this.repo.createQueryBuilder()
-          .select("*")
-          .where("make = :make", {make: estimateDto.make})
-          .getRawMany()
-    }
+  createEstimate({ make, model, lng, lat, year, mileage}: GetEstimateDto) {
+    return this.repo.createQueryBuilder()
+      .select("AVG(price)", "price")
+      .where("make = :make", { make })
+      .andWhere ("model = :model", { model })
+      .andWhere ("lng - :lng BETWEEN -5 and 5", { lng })
+      .andWhere ("lat - :lat BETWEEN -5 and 5", { lat })
+      .andWhere ("year - :year BETWEEN -3 and 3", { year })
+      .orderBy("ABS(mileage - :mileage)", "DESC")
+      .setParameters({ mileage })
+      .getRawOne();
+  }
 
-    create(reportDto: CreateReportDto, user: User) {
-        const report = this.repo.create(reportDto);
-        report.user = user;
-        return this.repo.save(report);
-    }
+  create(reportDto: CreateReportDto, user: User) {
+    const report = this.repo.create(reportDto);
+    report.user = user;
+    return this.repo.save(report);
+  }
 
-    async changeApproval(id: string, approved: boolean) {
-        const report = await this.repo.findOne(id);
-        if (!report) {
-            throw new NotFoundException("Report not found")
-        }
-        report.approved = approved;
-        return this.repo.save(report);
+  async changeApproval(id: string, approved: boolean) {
+    const report = await this.repo.findOne(id);
+    if (!report) {
+      throw new NotFoundException("Report not found");
     }
+    report.approved = approved;
+    return this.repo.save(report);
+  }
 }
