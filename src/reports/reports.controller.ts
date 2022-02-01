165a3e7011ffb1ba756c841a7483d8d4d149da13
Vlad Prodan <vladprodan6@gmail.com>
2022-02-01 11:36:24 +0200

    Creating a Query Builder

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,22 +1,23 @@
-import { Controller, Post, Body, UseGuards, Patch, Param, Query, Get } from '@nestjs/common';
-import { CreateReportDto } from './dtos/create-report.dto';
-import { ReportsService } from './reports.service';
-import { AuthGuard } from '../guards/auth.guard';
-import { CurrentUser } from '../users/decorators/current-user.decorator';
-import { User } from '../users/user.entity';
-import { ReportDto } from './dtos/report.dto';
-import { Serialize } from '../interceptors/serialize.interceptor';
-import {ApproveReportDto} from "./dtos/approve-report.dto";
-import { AdminGuard } from 'src/guards/admin.guard';
-import { GetEstimateDto } from './dtos/get-estimate.dto';
+import { Controller, Post, Body, UseGuards, Patch, Param, Query, Get } from "@nestjs/common";
+import { CreateReportDto } from "./dtos/create-report.dto";
+import { ReportsService } from "./reports.service";
+import { AuthGuard } from "../guards/auth.guard";
+import { CurrentUser } from "../users/decorators/current-user.decorator";
+import { User } from "../users/user.entity";
+import { ReportDto } from "./dtos/report.dto";
+import { Serialize } from "../interceptors/serialize.interceptor";
+import { ApproveReportDto } from "./dtos/approve-report.dto";
+import { AdminGuard } from "src/guards/admin.guard";
+import { GetEstimateDto } from "./dtos/get-estimate.dto";
 
-@Controller('reports')
+@Controller("reports")
 export class ReportsController {
-  constructor(private reportsService: ReportsService) {}
+  constructor(private reportsService: ReportsService) {
+  }
 
   @Get()
   getEstimate(@Query() query: GetEstimateDto) {
-    console.log(query);
+    return this.reportsService.createEstimate(query);
   }
 
   @Post()
